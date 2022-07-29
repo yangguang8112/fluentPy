8cf929ba0d60f4b35fb2a19e03239c62f40de473
yangguang <yangguang8112@mails.jlu.edu.cn>
2022-07-29 17:48:20 +0800

    chapter11.5

---
+++ chapter11/frenchdeck2.py
@@ -0,0 +1,25 @@
+import collections
+
+Card = collections.namedtuple('Card_name', ['rank', 'suit'])
+
+class FrenchDeck2(collections.MutableSequence):
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self) -> None:
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self) -> int:
+        return len(self._cards)
+    
+    def __getitem__(self, position):
+        return self._cards[position]
+    
+    def __setitem__(self, position, value):
+        self._cards[position] = value
+    
+    def __delitem__(self, position):
+        del self._cards[position]
+    
+    def insert(self, index: int, value) -> None:
+        self._cards.insert(index, value)
---
+++ vector2d_v0.py
@@ -0,0 +1,10 @@
+class Vector2d:
+    typecode = 'd'
+
+    def __init__(self, x, y) -> None:
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
