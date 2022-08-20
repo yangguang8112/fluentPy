a507f0c4033878aa164924e70213a30b8b673640
yangguang <yangguang8112@mails.jlu.edu.cn>
2022-08-20 15:10:00 +0800

    c16 s

---
+++ chapter16/coro_exec_demo.py
@@ -0,0 +1,52 @@
+
+"""
+>>> exc_coro = demo_exc_handling()
+>>> next(exc_coro)
+-> coroutine started
+>>> exc_coro.send(11)
+-> coroutine received: 11
+>>> exc_coro.send(22)
+-> coroutine received: 22
+>>> exc_coro.close()
+>>> from inspect import getgeneratorstate
+>>> getgeneratorstate(exc_coro)
+'GEN_CLOSED'
+
+>>> exc_coro = demo_exc_handling()
+>>> next(exc_coro)
+-> coroutine started
+>>> exc_coro.send(11)
+-> coroutine received: 11
+>>> exc_coro.throw(DemoException)
+*** DemoException handled. Continuing...
+>>> getgeneratorstate(exc_coro)
+'GEN_SUSPENDED'
+
+>>> exc_coro = demo_exc_handling()
+>>> next(exc_coro)
+-> coroutine started
+>>> exc_coro.send(11)
+-> coroutine received: 11
+>>> exc_coro.throw(ZeroDivisionError)
+Traceback (most recent call last):
+...
+ZeroDivisionError
+>>> getgeneratorstate(exc_coro)
+'GEN_CLOSED'
+"""
+
+class DemoException(Exception):
+    """为演示定义的异常类型"""
+
+
+
+def demo_exc_handling():
+    print('-> coroutine started')
+    while True:
+        try:
+            x = yield
+        except DemoException:
+            print('*** DemoException handled. Continuing...')
+        else:
+            print('-> coroutine received: {!r}'.format(x))
+    raise RuntimeError('This line should never run.')
---
+++ chapter16/coro_finally_demo.py
@@ -0,0 +1,57 @@
+
+"""
+>>> exc_coro = demo_exc_handling()
+>>> next(exc_coro)
+-> coroutine started
+>>> exc_coro.send(11)
+-> coroutine received: 11
+>>> exc_coro.send(22)
+-> coroutine received: 22
+>>> exc_coro.close()
+-> coroutine ending
+>>> from inspect import getgeneratorstate
+>>> getgeneratorstate(exc_coro)
+'GEN_CLOSED'
+
+>>> exc_coro = demo_exc_handling()
+>>> next(exc_coro)
+-> coroutine started
+>>> exc_coro.send(11)
+-> coroutine received: 11
+>>> exc_coro.throw(DemoException)
+*** DemoException handled. Continuing...
+>>> getgeneratorstate(exc_coro)
+'GEN_SUSPENDED'
+>>> exc_coro.close()
+-> coroutine ending
+
+>>> exc_coro = demo_exc_handling()
+>>> next(exc_coro)
+-> coroutine started
+>>> exc_coro.send(11)
+-> coroutine received: 11
+>>> exc_coro.throw(ZeroDivisionError)
+Traceback (most recent call last):
+...
+ZeroDivisionError
+>>> getgeneratorstate(exc_coro)
+'GEN_CLOSED'
+"""
+
+class DemoException(Exception):
+    """为演示定义的异常类型"""
+
+
+
+def demo_exc_handling():
+    print('-> coroutine started')
+    try:
+        while True:
+            try:
+                x = yield
+            except DemoException:
+                print('*** DemoException handled. Continuing...')
+            else:
+                print('-> coroutine received: {!r}'.format(x))
+    finally:
+        print('-> coroutine ending')
