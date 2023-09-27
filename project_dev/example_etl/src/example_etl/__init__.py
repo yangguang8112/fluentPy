0011b6d836ae3d87a3c6a9ca828603709049db5d
yangguang <yg948398165@gmail.com>
2023-09-27 15:50:10 +0800

    init project example_etl

---
+++ project_dev/example_etl/src/example_etl/__init__.py
@@ -0,0 +1,2 @@
+"""example-etl"""
+__version__ = '0.1.0'
---
+++ project_dev/example_etl/tests/test_version.py
@@ -0,0 +1,7 @@
+"""Tests"""
+from example_etl import __version__
+
+
+def test_version():
+    """Test version"""
+    assert __version__ == '0.1.0'
